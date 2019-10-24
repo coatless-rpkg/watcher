13de437a3ce09ef3ea3605ac9ad82c58a6c6fac9
James J Balamuta <balamut2@illinois.edu>
2019-10-24 10:01:10 -0500

    Add a series of watchers

---
+++ R/watchers.R
@@ -0,0 +1,29 @@
+watch_remove = function(hook_name) {
+    setHook(hook_name, NULL, "replace")
+}
+
+watch_register = function(x) {
+    hook_name = packageEvent(x, "attach")
+
+    # Verify hook hasn't been set yet.
+    # Hooks that are not set, return an empty list with length 0.
+    if (length(hook_name) != 0) {
+        remove_hook(hook_name)
+    }
+
+    # Establish a new hook
+    setHook(hook_name, function(...) {
+        packageStartupMessage("Detected package load...")
+        stop("The `", hook_name, "` package is not allowed.", call. = FALSE)
+    })
+}
+
+watch_begins = function() {
+    hooks_established = sapply(.pkgwatch$pkgs, FUN = watch_register)
+    invisible(hooks_established)
+}
+
+watch_ends = function() {
+    hooks_destroyed = sapply(.pkgwatch$pkgs, FUN = watch_remove)
+    invisible(hooks_destroyed)
+}
